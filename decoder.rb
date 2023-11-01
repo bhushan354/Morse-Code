0d716f1b71a1f04378f703ef104c88bef3549120
bhushan354 <deshmukhumesh354@gmail.com>
2023-11-01 21:35:22 +0530

    Fix sentence_decode method

--- decoder.rb
+++ decoder.rb
@@ -31,3 +31,13 @@ MORSE_TO_ENGLISH = {
 def character_decode(string)
   MORSE_TO_ENGLISH[string] || ' '
 end
+
+def string_decode(word)
+  decoded_chars = word.split.map { |char| character_decode(char) }
+  decoded_chars.join
+end
+
+def sentence_decode(message)
+  decoded_words = message.split('   ').map { |word| string_decode(word) }
+  decoded_words.join(' ')
+end
