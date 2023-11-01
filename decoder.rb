02737883716958878533ba19a04ba2a5dc953afd
bhushan354 <deshmukhumesh354@gmail.com>
2023-11-01 21:28:56 +0530

    Decode string

--- decoder.rb
+++ decoder.rb
@@ -1,71 +1,33 @@
+# Define a lookup table for Morse code to English letters
+MORSE_TO_ENGLISH = {
+  '.-' => 'A',
+  '-...' => 'B',
+  '-.-.' => 'C',
+  '-..' => 'D',
+  '.' => 'E',
+  '..-.' => 'F',
+  '--.' => 'G',
+  '....' => 'H',
+  '..' => 'I',
+  '.---' => 'J',
+  '-.-' => 'K',
+  '.-..' => 'L',
+  '--' => 'M',
+  '-.' => 'N',
+  '---' => 'O',
+  '.--.' => 'P',
+  '--.-' => 'Q',
+  '.-.' => 'R',
+  '...' => 'S',
+  '-' => 'T',
+  '..-' => 'U',
+  '...-' => 'V',
+  '.--' => 'W',
+  '-..-' => 'X',
+  '-.--' => 'Y',
+  '--..' => 'Z'
+}.freeze
+
 def character_decode(string)
-  case string
-  when ".-"
-   puts "A"
-  when "-..."
-   puts "B"
-  when "-.-."
-   puts "C"
-  when "-.."
-   puts "D"
-  when "."
-   puts "E"
-  when "..-."
-   puts "F"
-  when "--."
-   puts "G"
-  when "...."
-   puts "H"
-  when ".."
-   puts "I"
-  when ".---"
-   puts "J"
-  when "-.-"
-   puts "K"
-  when ".-.."
-   puts "L"
-  when "--"
-   puts "M"
-  when "-."
-   puts "N"
-  when "---"
-   puts "O"
-  when ".--."
-   puts "P"
-  when "--.-"
-   puts "Q"
-  when ".-."
-   puts "R"
-  when "..."
-   puts "S"
-  when "-"
-   puts "T"
-  when "..-"
-   puts "U"
-  when "...-"
-   puts "V"
-  when ".--"
-   puts "W"
-  when "-..-"
-   puts "X"
-  when "-.--"
-   puts "Y"
-  when "--.."
-   puts "Z"
-  else
-   puts " "
-  end
+  MORSE_TO_ENGLISH[string] || ' '
 end
-
-def string_decode(word)
-  decoded_chars = word.split(' ').map(|char| character_decode(char))
-  decoded_chars.join
-end
-
-def sentence_decode(message)
-  decoded_words = message.split('   ').map{|word| string_decode(word)}
-  decoded_words.join(' ')
-end
-
-# result = sentence_decode('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
-# puts result
